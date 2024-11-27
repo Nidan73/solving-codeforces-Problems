940f1ca4d64ba7de1cc3eeb0ca8fdd7784e9d372
Nidan73 <111710686+Nidan73@users.noreply.github.com>
2024-11-27 16:07:51 +0600

    Add files via upload

---
+++ Ultra-Fast Mathematician/Ultra-Fast-Mathematician.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+string x,y;
+cin>>x>>y;
+
+for(int i = 0; i<x.size();i++)
+
+    {
+        if(x[i] == y[i])
+         {
+           cout<<"0";
+
+        }
+        else
+        {
+           cout<<"1";
+
+        }
+
+    }
+
+    return 0;
+}
